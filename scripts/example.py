00ae4a0cbba24e65f9dad7406bb8d8e2f9aed0e4
hitorilabs <131238467+hitorilabs@users.noreply.github.com>
2024-06-10 22:39:37 +0000

    add simple inference example

--- scripts/example.py
+++ scripts/example.py
@@ -1,27 +1,40 @@
+import click
 import torch
 from safetensors.torch import load_model
 from bert.base_model import BertConfig, BertModel
 from transformers import AutoTokenizer
 from pathlib import Path
 
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-hf_model_path = Path("/home/bocchi/models/google-bert/bert-large-uncased-whole-word-masking")
-custom_model_path = Path("/home/bocchi/bert/models/google-bert/bert-large-uncased-whole-word-masking")
-tokenizer = AutoTokenizer.from_pretrained(hf_model_path)
 
-with torch.device(device):
-    config = BertConfig()
-    custom_model = BertModel(config)
-    missing, unexpected = load_model(custom_model, (custom_model_path / "model.safetensors").as_posix(), device="cuda")
-    if missing or unexpected:
-        print(missing, unexpected)
-    custom_model.eval()
+@click.command()
+@click.option("-p", "--path-to-hf", required=True, help="Path to local model directory with downloaded HF models")
+@click.option("-c", "--custom-model-path", default="./models", help="Path to custom model (from convert_hf.py)")
+def check_model(path_to_hf: str, custom_model_path: str):
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    hf_model_path = Path(path_to_hf)
+    model_path = Path(*hf_model_path.parts[-2:])
+    custom_model_path = Path(custom_model_path) / model_path
+    tokenizer = AutoTokenizer.from_pretrained(hf_model_path)
 
-with torch.no_grad():
-    data = tokenizer(
-        "example text",
-        return_tensors="pt",
-        return_token_type_ids=False,
-    ).to(device)
-    output = custom_model(**data)
-    print(output)
+    with torch.device(device):
+        config = BertConfig()
+        custom_model = BertModel(config)
+        missing, unexpected = load_model(
+            custom_model, (custom_model_path / "model.safetensors").as_posix(), device="cuda"
+        )
+        if missing or unexpected:
+            print(missing, unexpected)
+        custom_model.eval()
+
+    with torch.no_grad():
+        data = tokenizer(
+            "example text",
+            return_tensors="pt",
+            return_token_type_ids=False,
+        ).to(device)
+        output = custom_model(**data)
+        print(output)
+
+
+if __name__ == "__main__":
+    check_model()
